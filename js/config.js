38e566f5e6db85ac9415c31779754edaf95afe73
Raul Chavez <58048329+raulchavezjr7@users.noreply.github.com>
2025-03-23 23:01:32 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_jN7ZJhhw3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2sg1i4c1go862t5f4gn4v8kun5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
